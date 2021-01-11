d16c2dfedd27bc51692dd1f18e54a01394e02dd9
莫式东 <5542@MYJ-20200423JSI>
2021-01-11 09:39:22 +0800

    添加日志实体表

--- PSI.API/Filter/ActionLogAttribute.cs
+++ PSI.API/Filter/ActionLogAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PSI.Models.APIModels;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,25 +11,58 @@ using System.Web.Http.Filters;
 namespace PSI.API.Filter
 {
     public class ActionLogAttribute : ActionFilterAttribute
-    { 
+    {
+        /// <summary>
+        /// 全局请求ID号
+        /// </summary>
+        private static long ActionId { get; set; }
+        /// <summary>
+        /// 客户端IP地址
+        /// </summary>
+        private string ClientIp { get; set; }
+        /// <summary>
+        /// 请求ID号
+        /// </summary>
+        private long RequestId { get; set; }
+
+        /// <summary>
+        /// 请求的接口
+        /// </summary>
+        private string controllername { get; set; }
         private const string Key = "action";
         private bool _IsDebugLog = true;
+        private HttpWebAPILog log;
         public override void OnActionExecuting(HttpActionContext actionContext)
         {
+            log = new HttpWebAPILog();
+            RequestId = ActionId++;
+            var strary = actionContext.Request.RequestUri.AbsolutePath.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            controllername = strary[1];
+            ClientIp = ((HttpContextWrapper)actionContext.Request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            if (ClientIp == "::1")
+            {
+                ClientIp = "localhost";
+            }
+            var par = GetArguments(actionContext);
+            log.RequestArguments = par;
+            log.RequestIP = ClientIp ?? "localhost";
+            log.RequestMethod = actionContext.Request.Method.ToString();
+            log.RequestUri = actionContext.Request.RequestUri.ToString();
+            log.RequestDate = DateTime.Now;
             if (_IsDebugLog)
             {
                 Stopwatch stopWatch = new Stopwatch();
 
-                actionContext.Request.Properties[Key] = stopWatch;
+                actionContext.Request.Properties[Key] = stopWatch; 
 
-                string actionName = actionContext.ActionDescriptor.ActionName;
-
-                Debug.Print(Newtonsoft.Json.JsonConvert.SerializeObject(actionContext.ActionArguments));
+                Debug.Print(JsonConvert.SerializeObject(actionContext.ActionArguments));
 
                 stopWatch.Start();
             }
 
         }
+
+   
         public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
         {
             if (_IsDebugLog)
@@ -46,9 +81,66 @@ namespace PSI.API.Filter
                     Debug.Print(actionExecutedContext.Response.Content.ReadAsStringAsync().Result);
 
                     Debug.Print(string.Format(@"[{0}/{1} 用时 {2}ms]", controllerName, actionName, stopWatch.Elapsed.TotalMilliseconds));
+                    log.Milliseconds = stopWatch.Elapsed.TotalMilliseconds;
                 }
             }
         }
+
+        /// <summary>
+        /// 获取请求参数
+        /// </summary>
+        /// <param name="actionContext"></param>
+        /// <returns></returns>
+        private string GetArguments(HttpActionContext actionContext)
+        {
+            string postStr = "";
+            var test = actionContext.ActionArguments;
+            foreach (var b in test)
+            {
+                var post = actionContext.ActionArguments[b.Key];
+
+                if (null != post)
+                {
+                    Type t = post.GetType();
+                    var str = "";
+                    if (typeof(string) == t)
+                    {
+                        str = $"{b.Key}:{post}";
+                        str = "{" + str + "}";
+                    }
+                    else
+                    {
+                        var typeArr = t.GetProperties();
+
+                        foreach (var a in typeArr.OrderBy(x => x.Name))
+                        {
+                            var name = a.Name;
+                            var v = a.GetValue(post, null);
+                            if (v == null)
+                            {
+                                str += $"{name}:参数为null";
+                                continue;
+                            }
+                            string curreV = v.ToString();
+                            if (v.GetType() != typeof(string))
+                            {
+                                curreV = JsonConvert.SerializeObject(v);
+                            }
+                            if (null != v && v.ToString() != "")
+                            {
+                                str += @"""" + name + @""":" + @"""" + curreV + @""",";
+                            }
+                        }
+                        str = str.TrimEnd(',');
+                        str = @"{" + str + "}";
+                    }
+
+                    postStr += str + ",";
+                }
+            }
+            return postStr.TrimEnd(',');
+        }
+
 
     }
 }
---
+++ PSI.Models/APIModels/HttpWebAPILog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSI.Models.APIModels
+{
+	/// <summary>
+	/// 接口日志
+	/// </summary>
+    public class HttpWebAPILog
+    {
+		public string ID { set; get; }
+		public string RequestIP { set; get; }
+		public string RequestUri { set; get; }
+		public string RequestMethod { set; get; }
+		public string RequestArguments { set; get; }
+		public string ResponseResult { set; get; }
+		public string StateCode { set; get; }
+		public string Message { set; get; }
+		/// <summary>
+		/// 耗时 单位毫秒
+		/// </summary>
+		public double Milliseconds  { set; get; } 
+		public DateTime? RequestDate { set; get; }
+		public DateTime? ResponseDate { set; get; }
+	}
+}
